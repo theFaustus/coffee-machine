beb34fe9a5d1f682a932ea0df2ee243ff23a7d8c
ipascari <jhoonnyc@gmail.com>
2020-12-13 19:58:20 +0200

    initial commit

---
+++ src/test/java/com/evil/inc/coffeemachine/CoffeeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.inc.coffeemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
